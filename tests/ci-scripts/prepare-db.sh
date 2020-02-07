a437c57d5dca7c8b48c2e1ad4eb825c4e9f1200a
Brian Hoffman <brianjhoffman@gmail.com>
2020-02-06 21:06:20 -0500

    travis test 3

--- tests/ci-scripts/prepare-db.sh
+++ tests/ci-scripts/prepare-db.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
+mysql -e 'CREATE DATABASE `ojs` DEFAULT CHARACTER SET utf8'
 
-sudo service mysql start
-sudo mysql -u root -e 'CREATE DATABASE `ojs` DEFAULT CHARACTER SET utf8'
+mysql -e "GRANT ALL ON \`ojs\`.* TO \`ojs\`@localhost IDENTIFIED BY 'ojs'"
 
-sudo mysql -u root -e "GRANT ALL ON \`ojs\`.* TO \`ojs\`@localhost IDENTIFIED BY 'ojs'"
+mysql -u root ojs < tests/test_db.sql
 
-sudo mysql -u root -p ojs < tests/test_db.sql
-
-export DBTYPE=MySQLi
 
